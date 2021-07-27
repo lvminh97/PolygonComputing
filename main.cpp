8fd52b6e6c32ed79cb5c8b1e2e81782078d35be7
lvminh97 <lvminh97@gmail.com>
2021-07-27 12:08:13 +0700

    Init import

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Point{
+    double x, y;  
+};
+
+struct LineSegment{
+    Point p, q;
+};
+
+void readFile(){
+    
+}
+
+int main(){
+
+    return 0;
+}
